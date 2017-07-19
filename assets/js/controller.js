020fd05c5dbc5199005bf4a333727b5f00b3637a
Mahabub <mahbubh209@gmail.com>
2017-07-19 00:49:16 -0500

    update controller

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -2,21 +2,24 @@
 app.controller('weatherCtrl', ['$scope', 'weatherService', function($scope, weatherService) {
 	$scope.place=[];
 	$scope.placeDetails = [];
-	$scope.fiveDayTemp = [];
+	//$scope.fiveDayTemp = "";
+	$scope.fiveDayAvgTemp = 0;
 	
 	function fetchWeatherDetails(woeid){
 		weatherService.getWeatherDetails(woeid).then(function(data){
 			$scope.placeDetails = data;
-			console.log("in fetchWeatherDetails controller");
-			console.log($scope.placeDetails);
-			//$scope.fiveDayTemp = [data.consolidated_weather[0].the_temp,data.consolidated_weather[1].the_temp,data.consolidated_weather[2].the_temp,data.consolidated_weather[3].the_temp,data.consolidated_weather[4].the_temp];
+			//console.log("in fetchWeatherDetails controller");
+			//console.log(data.consolidated_weather[0].the_temp + " - " + data.consolidated_weather[1].the_temp + " - " + data.consolidated_weather[2].the_temp + " - " + data.consolidated_weather[3].the_temp + " - " + data.consolidated_weather[4].the_temp);
+			$scope.fiveDayAvgTemp = (data.consolidated_weather[0].the_temp+data.consolidated_weather[1].the_temp+data.consolidated_weather[2].the_temp+data.consolidated_weather[3].the_temp+data.consolidated_weather[4].the_temp)/5;
+			$scope.fiveDayTemp = data.consolidated_weather[0].the_temp + "," + data.consolidated_weather[1].the_temp + "," + data.consolidated_weather[2].the_temp + "," + data.consolidated_weather[3].the_temp + "," + data.consolidated_weather[4].the_temp;
 		});
 	}
 	function fetchWoeid(city) {
 		weatherService.getWoeid(city).then(function(data){
 			$scope.place = data;
-			console.log("in controller");
-			console.log($scope.place[0].title);
+			//console.log("in controller");
+			//console.log($scope.place[0].title);
+			
 			fetchWeatherDetails($scope.place[0].woeid);
 		});
 	}
